25a6b4400923b59274ec9eb5cb53d1366f0e8b95
Luis Hernandez <luiszah36@gmail.com>
2023-04-24 12:42:21 -0400

    Adding connection to database with mongoose

--- models/server.js
+++ models/server.js
@@ -1,28 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config.database')
 
-class Server{
+class Server {
 
-constructor(){
-    this.app = express();
-    this.port = process.env.PORT || 8080;
-    this.usuarioPath = '/api/users';
-    this.middleware();   
-    this.routes();
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 8080;
+        this.usuarioPath = '/api/users';
+        this.conectarDB();
+        this.middleware();
+        this.routes();
 
-}
-    middleware(){
-        this.app.use(cors());
-        this.app.use(express.static('public')); 
-        this.app.use(express.json()); 
     }
 
-    routes(){
+    async conectarDB() {
+        dbConnection();
+    }
+
+    middleware() {
+        this.app.use(cors());
+        this.app.use(express.static('public'));
+        this.app.use(express.json());
+    }
+
+    routes() {
         this.app.use(this.usuarioPath, require('../routes/users'));
     }
 
-    listen(){
-        this.app.listen(this.port, ()=>{
+    listen() {
+        this.app.listen(this.port, () => {
             console.log(this.port)
         })
     }
